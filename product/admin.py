7b126a65f338256de00df389659d694700686c67
NirajanJoc <Nirajanjoc19@gmail.com>
2022-05-16 23:46:19 +0545

    'migration_models'

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from product.models import Product, Weight
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','quantity','status','weight')
+
+@admin.register(Weight)
+class WeightAdmin(admin.ModelAdmin):
+    list_display=('weight_range','price')
--- product/models.py
+++ product/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
 # Create your models here.
+class Weight(models.Model):
+    weight_range=models.CharField(max_length=255, unique=True)
+    price=models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.weight_range} / {self.price}"
+    
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+class Product(models.Model):
+    STATUS_CHOICE=(
+        ('pending',"PENDING"),
+        ('delivered','DELIVERED'),
+        ('return','RETURN')
+    )
+    weight=models.ForeignKey(Weight, on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    price=models.CharField(max_length=255)
+    quantity=models.IntegerField(max_length=255)
+    category=models.CharField(max_length=255)
+    fullname=models.CharField(max_length=255)
+    contact=models.CharField(max_length=255)
+    address=models.CharField(max_length=255)
+    status=models.CharField(max_length=50, choices=STATUS_CHOICE)
+    shipping_address=models.CharField(max_length=255, null=True, blank=True)
+    remarks=models.TextField(null=True, blank=True )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.status='pending'
+        super().save(*args, **kwargs)
