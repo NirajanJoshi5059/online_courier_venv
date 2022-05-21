a5c8ca1c8d1c351c7bc7a133d11b332fbb1a947d
NirajanJoc <Nirajanjoc19@gmail.com>
2022-05-21 23:24:19 +0545

    'forms-model'

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from product.views import product_list
+from product.views import product_list,dashboard,product_form
 
 
 app_name='product'
 
 urlpatterns=[
     path('product-list/', product_list, name='product_list'),
+    path('',dashboard,name='dashboard'),
+    path('save-product',product_form, name='product_form'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from product.forms import ProductForm
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'dashboard.html')
+    
 def product_list(request):
     return render(request, 'product_list.html')
+
+def product_form(request):
+    # if request.POST:
+    #     product_name=request.POST['product_name']
+    #     print(product_name)
+    form=ProductForm(request.POST or None)
+    context={'form':form}
+    return render(request, 'form.html',context)
